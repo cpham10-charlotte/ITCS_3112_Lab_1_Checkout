f660fcf52e0c0e94407bb5f6008353d6d232a9f0
Colin Pham <cpham10@charlotte.edu>
2026-02-20 15:19:43 -0500

    CheckoutRecord set up

--- Domain/CheckoutRecord.cs
+++ Domain/CheckoutRecord.cs
@@ -2,5 +2,17 @@ namespace ITCS_3112_Lab_1_Checkout.Domain;
 
 public class CheckoutRecord
 {
-    
+    public string Id { get; init; }
+    public List<Item> Items { get; init; }
+    public Borrower Borrower { get; init; }
+    public DateTime Date { get; init; }
+    public DateTime DueDate { get; init; }
+
+    public CheckoutRecord(string id, List<Item> items, Borrower borrower)
+    {
+        this.Id = id;
+        this.Items = items;
+        this.Borrower = borrower;
+        this.Date = DateTime.Now;
+    }
 }
