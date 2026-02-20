9a0c263c67a6166f87a90a8e6b35cdcf0adff511
Colin Pham <cpham10@charlotte.edu>
2026-02-20 15:46:17 -0500

    Borrower comments added and LogRecord method created

--- Domain/Borrower.cs
+++ Domain/Borrower.cs
@@ -1,5 +1,8 @@
 namespace ITCS_3112_Lab_1_Checkout.Domain;
 
+/// <summary>
+/// Class for Individuals who borrow and return items
+/// </summary>
 public class Borrower
 {
     public string Id { get; init; }
@@ -17,13 +20,30 @@ public class Borrower
         this.BorrowHistory = new List<CheckoutRecord>();
     }
 
+    /// <summary>
+    /// Adds item to borrowers item list
+    /// </summary>
+    /// <param name="item">Item being borrowed</param>
     public void BorrowItem(Item item)
     {
         this.BorrowedItems.Add(item);
     }
     
+    /// <summary>
+    /// Removes item from borrowers item list 
+    /// </summary>
+    /// <param name="item">Item being returned</param>
     public void ReturnItem(Item item)
     {
         this.BorrowedItems.Remove(item);
+    }
+
+    /// <summary>
+    /// Logs record after borrower borrows an item
+    /// </summary>
+    /// <param name="record">Record from transaction</param>
+    public void LogRecord(CheckoutRecord record)
+    {
+        this.BorrowHistory.Add(record);
     }
 }
