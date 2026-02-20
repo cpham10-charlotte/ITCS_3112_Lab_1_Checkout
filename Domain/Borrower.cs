98e6a6ece4927e71bb5502060e07ed5bf0bbd403
Colin Pham <cpham10@charlotte.edu>
2026-02-20 15:32:48 -0500

    Borrower set up

--- Domain/Borrower.cs
+++ Domain/Borrower.cs
@@ -2,5 +2,28 @@ namespace ITCS_3112_Lab_1_Checkout.Domain;
 
 public class Borrower
 {
+    public string Id { get; init; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public List<Item> BorrowedItems { get; set; }
+    public List<CheckoutRecord> BorrowHistory { get; set; }
+
+    public Borrower(string id, string name, string email)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Email = email;
+        this.BorrowedItems = new List<Item>();
+        this.BorrowHistory = new List<CheckoutRecord>();
+    }
+
+    public void BorrowItem(Item item)
+    {
+        this.BorrowedItems.Add(item);
+    }
     
+    public void ReturnItem(Item item)
+    {
+        this.BorrowedItems.Remove(item);
+    }
 }
