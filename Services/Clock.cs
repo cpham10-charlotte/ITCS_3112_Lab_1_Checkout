46ca1222ee9510396a4faf2c08288ad1dceb034a
Carolina <181431400+carolinahrnd3z@users.noreply.github.com>
2026-02-19 23:50:49 -0500

    Update Clock and Notifier in Service

--- Services/Clock.cs
+++ Services/Clock.cs
@@ -1,6 +1,11 @@
+using ITCS_3112_Lab_1_Checkout.Contracts;
+
 namespace ITCS_3112_Lab_1_Checkout.Services;
 
-public class Clock
+public class Clock : IClock
 {
-    
+    public DateTime Today()
+    {
+        return DateTime.Today;
+    }
 }
--- Services/Notifier.cs
+++ Services/Notifier.cs
@@ -1,6 +1,18 @@
+using ITCS_3112_Lab_1_Checkout.Contracts;
+using ITCS_3112_Lab_1_Checkout.Domain;
+
+
 namespace ITCS_3112_Lab_1_Checkout.Services;
 
-public class Notifier
+public class Notifier : INotifier
 {
-    
+    public void DueSoon(Borrower borrower, CheckoutRecord record)
+    {
+        Console.WriteLine($"Reminder: {borrower.Name}, item{record.ItemId} is due on {record.DueDate:d}");
+    }
+
+    public void Overdue(Borrower borrower, CheckoutRecord record)
+    {
+        Console.WriteLine($"OVERDUE: {borrower.Name}, item{record.ItemId} was due on {record.DueDate:d}");
+    }
 }
